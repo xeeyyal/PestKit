7877219abe50f5ce77efc6796cf591f2fea75ac8
Gunell8 <gvlyfa@gmail.com>
2023-12-12 13:31:00 +0400

    CRUD complated

--- Areas/Admin/Controllers/ProjectController.cs
+++ Areas/Admin/Controllers/ProjectController.cs
@@ -33,21 +33,34 @@ namespace PestKitAB104.Areas.Admin.Controllers
         }
 
         [HttpPost]
-
         public async Task<IActionResult> Create(CreateProjectVM projectVM)
         {
-            if (!ModelState.IsValid) return View();
+             if (!ModelState.IsValid) return View();
 
-            bool result = _context.Projects.Any(a => a.Name.Trim() == projectVM.Name.Trim());
+            bool result = _context.Projects.Include(p=>p.ProjectImages.Where(pi=>pi.IsPrimary==true)).Any(a => a.Name.Trim() == projectVM.Name.Trim());
             if (result)
             {
                 ModelState.AddModelError("Name", "Bu adda project movcuddur");
                 return View();
             }
 
+            string filename = await projectVM.MainPhoto.CreateFile(_env.WebRootPath, "img");
+
+            ProjectImage mainimage = new ProjectImage
+            {
+                IsPrimary = true,
+                ImgUrl = await projectVM.MainPhoto.CreateFile(_env.WebRootPath, "img")
+            };
+
+            List<ProjectImage> images;
+
             Project project = new Project
             {
-                Name = projectVM.Name
+                Name = projectVM.Name,
+                ProjectImages= new List<ProjectImage> 
+                {
+                    mainimage
+                }
             };
 
             await _context.Projects.AddAsync(project);
@@ -59,29 +72,30 @@ namespace PestKitAB104.Areas.Admin.Controllers
         {
             if (id <= 0) return BadRequest();
 
-            Project project = await _context.Projects.Include(p=>p.ProjectImages.Where(pi=>pi.IsPrimary==true)).FirstOrDefaultAsync(d => d.Id == id);
+            Project? project = await _context.Projects.Include(x => x.ProjectImages).FirstOrDefaultAsync(x => x.Id == id);
 
-            if (project is null) return NotFound();
+            if (project is null) { return NotFound(); }
 
             UpdateProjectVM projectVM = new UpdateProjectVM
             {
                 Name = project.Name,
                 ProjectImages = project.ProjectImages
             };
+
             return View(projectVM);
         }
         [HttpPost]
-
         public async Task<IActionResult> Update(int id, UpdateProjectVM projectVM)
         {
-            Project existed = await _context.Projects
+            Project? existed = await _context.Projects
                 .Include(pi => pi.ProjectImages)
-
                 .FirstOrDefaultAsync(x => x.Id == id);
 
             projectVM.ProjectImages = existed.ProjectImages;
-            if (!ModelState.IsValid) { return View(projectVM); }
-            if (existed is null) { return NotFound(); }
+
+            if (!ModelState.IsValid) return View(projectVM);
+
+            if (existed is null) return NotFound();
 
             if (projectVM.MainPhoto is not null)
             {
@@ -90,13 +104,12 @@ namespace PestKitAB104.Areas.Admin.Controllers
                     ModelState.AddModelError("Photo", "File Not supported");
                     return View(projectVM);
                 }
-                if (!projectVM.MainPhoto.ValidateType())
-                {
-                    ModelState.AddModelError("Photo", "Image should not be larger than 10 mb");
-                    return View(projectVM);
-                }
+                //if (!projectVM.MainPhoto.ValidateSize(10))
+                //{
+                //    ModelState.AddModelError("Photo", "Image should not be larger than 10 mb");
+                //    return View(projectVM);
+                //}
             }
-
 
             if (projectVM.MainPhoto is not null)
             {
@@ -113,13 +126,14 @@ namespace PestKitAB104.Areas.Admin.Controllers
                 });
             }
 
-
-
-            if (existed.ProjectImages is null) { existed.ProjectImages = new List<ProjectImage>(); }
+            if (existed.ProjectImages is null) 
+            { 
+                existed.ProjectImages = new List<ProjectImage>(); 
+            }
 
             if (projectVM.ImageIds is null) projectVM.ImageIds = new List<int>();
 
-            List<ProjectImage> remove = existed.ProjectImages.Where(pi => pi.IsPrimary == null && !projectVM.ImageIds.Exists(imgId => imgId == pi.Id)).ToList();
+            List<ProjectImage> remove = existed.ProjectImages.Where(pi => pi.IsPrimary == null).ToList();
 
             foreach (ProjectImage image in remove)
             {
@@ -139,7 +153,7 @@ namespace PestKitAB104.Areas.Admin.Controllers
                         continue;
                     }
 
-                    if (!photo.ValidateSize(10))
+                    if (!photo.ValidateSize(20000))
                     {
                         TempData["Message"] += $"<p class=\"text-danger\">{photo.Name} the size is not suitable</p>";
                         continue;
@@ -151,8 +165,8 @@ namespace PestKitAB104.Areas.Admin.Controllers
                         ImgUrl = await photo.CreateFile(_env.WebRootPath, "img")
                     });
                 }
+                _context.Projects.Update(existed);
             }
-
             existed.Name = projectVM.Name;
 
             await _context.SaveChangesAsync();
@@ -163,22 +177,30 @@ namespace PestKitAB104.Areas.Admin.Controllers
         {
             if (id <= 0) return BadRequest();
 
-            Project existed = await _context.Projects.FirstOrDefaultAsync(a => a.Id == id);
+            Project project = await _context.Projects.Include(pi => pi.ProjectImages).FirstOrDefaultAsync(p => p.Id == id);
 
-            if (existed is null) return NotFound();
+            if (project is null) return NotFound();
 
-            _context.Projects.Remove(existed);
+            foreach (ProjectImage image in project.ProjectImages)
+            {
+                image.ImgUrl.DeleteFile(_env.WebRootPath, "img");
+            }
+
+            List<ProjectImage> remove = await _context.ProjectImages.Where(p => p.ProjectId == id).ToListAsync();
+
+            _context.ProjectImages.RemoveRange(remove);
+            _context.Projects.Remove(project);
             await _context.SaveChangesAsync();
+
             return RedirectToAction(nameof(Index));
         }
         public async Task<IActionResult> Details(int id)
         {
             if (id <= 0) return BadRequest();
 
-            Project project = await _context.Projects
-                .Include(p => p.ProjectImages.Where(pi => pi.IsPrimary == true))
-                .FirstOrDefaultAsync(d => d.Id == id);
-            if (project == null) return NotFound();
+            Project project = await _context.Projects.Include(p => p.ProjectImages).FirstOrDefaultAsync(p => p.Id == id);
+
+            if (project is null) return NotFound();
 
             return View(project);
         }
---
+++ Migrations/20231212074410_createUrlColumnInProjectsTable.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PestKitAB104.Migrations
+{
+    public partial class createUrlColumnInProjectsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Url",
+                table: "Projects",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Url",
+                table: "Projects");
+        }
+    }
+}
